bd79751baa64f56dc5aca5df9a8889a0aa582589
Faysal-Maxamed <pheymoha17@gmail.com>
2025-01-14 10:00:51 +0300

    restaurant backend created

---
+++ index.js
@@ -0,0 +1,20 @@
+import  express  from "express";
+import connectTodb from "./Configdb/databse.js";
+import userRoutes from './routes/userRoutes.js'
+
+
+
+const app = express()
+connectTodb()
+
+const port = 5000
+app.use(express.json())
+app.use('/api/users', userRoutes)
+
+app.listen(port , ()=>{
+    console.log(`Server is running on ${port}`)
+})
+
+app.get('/',(req,res)=>{
+    res.send("kusoo dhawoow")
+})
